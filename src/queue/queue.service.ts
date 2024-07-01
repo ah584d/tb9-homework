7b611406d90eb8fa2bff5ad5cca60279fa491e66
Avraham Hamu <ahamu@chegg.com>
2024-07-01 12:45:26 +0300

    implementing queue of client after amir Remark

--- src/queue/queue.service.ts
+++ src/queue/queue.service.ts
@@ -6,7 +6,6 @@ import { v4 as uuidv4 } from "uuid";
 export class QueueService {
   private readonly queueInstance = {};
   private readonly clientQueue: ClientQueue[] = [];
-  private intervalRefs = {};
 
   addMessage(queue_name: string, payload: unknown) {
     if (!this.queueInstance[queue_name]) {
@@ -14,6 +13,9 @@ export class QueueService {
     }
     this.queueInstance[queue_name].push(payload);
     console.log(`===> DEBUG after push: ${JSON.stringify(this.queueInstance)}`);
+
+    // Note Amir: here I release the response for the clients which wait for a response
+    this.processPendingRequests(queue_name);
   }
 
   async getMessage(
@@ -21,6 +23,7 @@ export class QueueService {
     timeout: number
   ): Promise<string | undefined> {
     return new Promise((res, _err) => {
+        // happy scenario, the queue has some elements to return
       if (this.queueInstance[queue_name]?.length > 0) {
         const lastElement = this.queueInstance[queue_name].shift();
         console.log(
@@ -28,25 +31,56 @@ export class QueueService {
         );
         res(lastElement);
       } else {
-        addClientRequest(queue_name);
-        setInterval(() => {
-          if (!this.queueInstance[queue_name]) {
-            return;
-          } else {
-            const lastElement = this.queueInstance[queue_name].shift();
-            res(lastElement);
-          }
-        }, 1000);
+        this.addClientRequest(queue_name, res);
+        setTimeout(() => {
+          this.resolveTimeout(queue_name, res);
+        }, timeout);
       }
     });
   }
-}
 
-function addClientRequest(queue_name: string): void {
-  const timestamp = Date.now();
-  const id = uuidv4();
-  this.clientQueue.push({ id, timestamp, queue_name });
-  this.clientQueue.sort(
-    (a: ClientQueue, b: ClientQueue) => b.timestamp - a.timestamp
-  );
+  private processPendingRequests(queue_name: string): void {
+    while (
+      this.queueInstance[queue_name]?.length > 0 &&
+      this.clientQueue[queue_name]?.length > 0
+    ) {
+      const nextRequest = this.clientQueue[queue_name].shift();
+      const lastElement = this.queueInstance[queue_name].shift();
+      if (nextRequest) {
+        nextRequest.resolve(lastElement);
+        console.log(
+          `===> DEBUG after resolving: ${JSON.stringify(this.queueInstance)}`
+        );
+      }
+    }
+  }
+
+  private addClientRequest(
+    queue_name: string,
+    resolve: (value?: unknown) => void
+  ): void {
+    const timestamp = Date.now();
+    const id = uuidv4();
+    if (!this.clientQueue[queue_name]) {
+      this.clientQueue[queue_name] = [];
+    }
+    this.clientQueue[queue_name].push({ id, timestamp, resolve });
+    this.clientQueue[queue_name].sort(
+      (a: ClientQueue, b: ClientQueue) => b.timestamp - a.timestamp
+    );
+    console.log(`===> DEBUG clientQueue: ${JSON.stringify(this.clientQueue)}`);
+  }
+
+  private resolveTimeout(
+    queue_name: string,
+    resolve: (value?: unknown) => void
+  ): void {
+    const index = this.clientQueue[queue_name]?.findIndex(
+      (request: PromiseConstructor) => request.resolve === resolve
+    );
+    if (index !== undefined && index >= 0) {
+      this.clientQueue[queue_name].splice(index, 1);
+      resolve(undefined);
+    }
+  }
 }
